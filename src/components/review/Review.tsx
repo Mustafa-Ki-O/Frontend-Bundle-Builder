0e345f60ffc876d1a08c1d29e092691b5ca60d30
Mustafa-Ki-O <robnsonmark11@gmail.com>
2026-07-27 17:25:59 +0300

    inialize colors and ui architecture

---
+++ src/components/review/Review.tsx
@@ -0,0 +1,21 @@
+const Review = () => {
+
+    return (
+        <div className="pt-[15px] max-w-[399px] flex flex-col justify-center items-start bg-background rounded-[10px]">
+        <h5 className="px-15 uppercase text-step text-xs">
+            Review
+        </h5>
+
+        <div className="flex flex-col justify-center items-start p-20 pb-31">
+        <h5 className=" text-[#1F1F1F] text-[22px] font-semibold">
+            Your security system
+        </h5>
+        <h5 className=" text-text text-[14px]">
+            Review your personalized protection system designed to keep what matters most safe.
+        </h5>
+        </div>
+
+        </div>
+    )
+}
+export default Review
---
+++ src/components/steps/Steps.tsx
@@ -0,0 +1,21 @@
+const Steps = () => {
+
+
+    return (
+        <section className="flex-1  bg-background rounded-[10px]">
+           <h5 className="px-15 uppercase text-step text-xs">
+            Review
+        </h5>
+
+        <div className="flex flex-col justify-center items-start p-20 pb-31">
+        <h5 className=" text-[#1F1F1F] text-[22px] font-semibold">
+            Your security system
+        </h5>
+        <h5 className=" text-text text-[14px]">
+            Review your personalized protection system designed to keep what matters most safe.
+        </h5>
+        </div>
+        </section>
+    )
+}
+export default Steps
